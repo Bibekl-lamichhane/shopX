17578b81255af769efa57d3f688f4e3350261b72
Bibek Lamichhane <bibek1999lamichhane@gmail.com>
2025-06-27 08:24:11 +0545

    added AdvanceJS

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const play=(()=>{
-    console.log("hello guys")
-})
+function getData(getId, getNextData) {
+  setTimeout(() => {
+    console.log("data", getId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 3000);
+}
+getData(1, () => {
+  console.log("Fetching data 2....");
+  getData(2, () => {
+    console.log("Fetching data 3...");
+    getData(3, () => {
+      console.log("Fetching data 4...");
+      getData(4);
+    });
+  });
+});
